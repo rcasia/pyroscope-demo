e6f7fea6ab59eb39a522be434505cd202843b4f5
Ricardo Casía <ricardocasia.dev@gmail.com>
2024-04-27 20:23:31 +0200

    init spring application

---
+++ src/main/java/com/example/pyroscopedemo/PyroscopeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pyroscopedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyroscopeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyroscopeDemoApplication.class, args);
+	}
+
+}
